688108f12236eda1a202097d1593bcdcb26521a5
Nelson Loyola <nloyola@gmail.com>
2014-04-18 14:27:06 -0600

    more refactoring

--- app/org/biobank/service/Processor.scala
+++ app/org/biobank/service/Processor.scala
@@ -12,33 +12,22 @@ import Scalaz._
 
 trait Processor extends EventsourcedProcessor with ActorLogging {
 
+  /** Persists the event passed in the validation if it is successful. In either case
+    *  the sender is sent either the success or failure validation.
+    */
   protected def process[T](validation: DomainValidation[T])(onSuccess: T => Unit) {
-    validation foreach { event =>
+    validation map { event =>
       persist(event) { e =>
 	onSuccess(e)
+	// inform the sender of the successful command with the event
 	sender ! e.success
       }
     }
 
     if (validation.isFailure) {
+      // inform the sender of the failure
       sender ! validation
     }
   }
 
-  protected def logEvent[T](validation: DomainValidation[T]) {
-    if (log.isDebugEnabled) {
-      validation match {
-        case Success(item) =>
-          log.debug("%s".format(item))
-        case Failure(msglist) =>
-          log.debug("msg: %s".format(msglist.head))
-      }
-    }
-  }
-
-  protected def logCommand[T](processorName: String, cmd: Any) {
-    if (log.isDebugEnabled) {
-      log.debug("%s: %s".format(processorName, cmd))
-    }
-  }
 }
