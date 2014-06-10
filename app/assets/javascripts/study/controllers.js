f655674a9c800ec722212841e8d8fb7624338532
Nelson Loyola <loyola@ualberta.ca>
2014-06-10 14:56:13 -0600

    Fix for page title

--- app/assets/javascripts/study/controllers.js
+++ app/assets/javascripts/study/controllers.js
@@ -7,7 +7,8 @@ define(['angular'], function(angular) {
   /**
    * user is not a service, but stems from userResolve (Check ../user/services.js) object.
    */
-  var StudiesCtrl = function($scope, $location, user, studyService) {
+  var StudiesCtrl = function($scope, $rootScope, $location, user, studyService) {
+    $rootScope.pageTitle = 'Biobank studies';
     studyService.list().then(function(response) {
       $scope.studies = response.data;
       $scope.user = user;
@@ -21,7 +22,8 @@ define(['angular'], function(angular) {
   /**
    * See http://stackoverflow.com/questions/22881782/angularjs-tabset-does-not-show-the-correct-state-when-the-page-is-reloaded
    */
-  var StudyCtrl = function($scope, $location, user, studyService) {
+  var StudyCtrl = function($scope, $rootScope, $location, user, studyService) {
+    $rootScope.pageTitle = 'Biobank study';
     studyService.query().then(function(response) {
       $scope.study = response.data;
       $scope.user = user;
