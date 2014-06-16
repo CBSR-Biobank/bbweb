ba0b0ae449c2e31a146bd1c638751987ac1551d0
Nelson Loyola <nloyola@gmail.com>
2014-06-15 22:13:04 -0600

    implemented more security for REST api

--- app/org/biobank/controllers/Security.scala
+++ app/org/biobank/controllers/Security.scala
@@ -1,12 +1,15 @@
 package org.biobank.controllers
 
-import org.biobank.domain.UserId
+import org.biobank.domain.{ DomainValidation, DomainError, UserId }
 
 import scala.concurrent.Future
 import play.api._
 import play.api.mvc._
 import play.api.libs.json._
 import play.api.cache._
+
+import scalaz._
+import scalaz.Scalaz._
 
 /**
  * Security actions that should be used by all controllers that need to protect their actions.
@@ -20,34 +23,65 @@ trait Security { self: Controller =>
   val AuthTokenCookieKey = "XSRF-TOKEN"
   val AuthTokenUrlKey = "auth"
 
-  private def getToken[A](request: Request[A]) = {
-    val maybeToken = request.headers.get(AuthTokenHeader).orElse(request.getQueryString(AuthTokenUrlKey))
-    maybeToken flatMap { token =>
-      Some(token)
+  sealed case class AuthenticationInfo(token: String, userId: UserId)
+
+  /*
+   * Checks that the token is:
+   *
+   *  - present in the cookie header of the request,
+   *
+   *  - either in the header or in the query string,
+   *
+   *  - matches a token already stored in the play cache
+   */
+  private def validateToken[A](request: Request[A]): DomainValidation[AuthenticationInfo] = {
+    val xsrfTokenCookieOption = request.cookies.get(AuthTokenCookieKey)
+    val headerTokenOption = request.headers.get(AuthTokenHeader).orElse(request.getQueryString(AuthTokenUrlKey))
+
+    if (xsrfTokenCookieOption == None) {
+      DomainError("Invalid XSRF Token cookie").failNel
+    } else if (headerTokenOption == None) {
+      DomainError("No Token").failNel
+    } else {
+      val xsrfTokenCookie = xsrfTokenCookieOption.get
+      val token = headerTokenOption.get
+
+      val userIdOption = Cache.getAs[UserId](token)
+
+      if (userIdOption == None) {
+        DomainError("Token not found in cache").failNel
+      } else {
+        val userId = userIdOption.get
+
+        if (xsrfTokenCookie.value.equals(token)) {
+          AuthenticationInfo(token, userId).successNel
+        } else {
+          DomainError("Token does not match cookie").failNel
+        }
+      }
     }
   }
 
-  /** Checks that a token is either in the header or in the query string */
+  /**
+   * Ensures that the request has the proper authorization.
+   *
+   */
   def AuthAction[A](p: BodyParser[A] = parse.anyContent)(
-    f: String => UserId => Request[A] => Result): Action[A] = {
-    Action(p) { implicit request =>
-      getToken(request).flatMap { token =>
-        Cache.getAs[UserId](token) map { userId =>
-          f(token)(userId)(request)
-        }
-      } getOrElse Unauthorized(Json.obj("message" -> "No Token"))
-    }
+    f: String => UserId => Request[A] => Result): Action[A] = Action(p) { implicit request =>
+    validateToken(request).fold(
+      err => Unauthorized(Json.obj("message" -> err.list.mkString(", "))),
+      authInfo => f(authInfo.token)(authInfo.userId)(request))
   }
 
-  /** Checks that a token is either in the header or in the query string */
+  /**
+   * Ensures that the request has the proper authorization.
+   *
+   */
   def AuthActionAsync[A](p: BodyParser[A] = parse.anyContent)(
-    f: String => UserId => Request[A] => Future[Result]) = {
-    Action.async(p) { request =>
-      getToken(request).flatMap { token =>
-        Cache.getAs[UserId](token) map { userId =>
-          f(token)(userId)(request)
-        }
-      } getOrElse Future.successful(Unauthorized(Json.obj("message" -> "No Token")))
-    }
+    f: String => UserId => Request[A] => Future[Result]) = Action.async(p) { implicit request =>
+    validateToken(request).fold(
+      err => Future.successful(Unauthorized(Json.obj("message" -> err.list.mkString(", ")))),
+      authInfo => f(authInfo.token)(authInfo.userId)(request))
   }
+
 }
