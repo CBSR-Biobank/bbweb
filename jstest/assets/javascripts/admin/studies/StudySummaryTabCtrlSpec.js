da8c0e2ee51a6f762e2ed2426f5276d079745c36
Nelson Loyola <nloyola@gmail.com>
2014-11-19 13:25:50 -0700

    Client: added more tests.

--- jstest/assets/javascripts/admin/studies/StudySummaryTabCtrlSpec.js
+++ jstest/assets/javascripts/admin/studies/StudySummaryTabCtrlSpec.js
@@ -4,13 +4,21 @@ define(['angular', 'angularMocks', 'underscore', 'biobankApp'], function(angular
   'use strict';
 
   describe('Controller: StudySummaryTabCtrl', function() {
-    var scope;
+    var studiesService, modalService, scope;
     var study = {name: 'ST1', description: 'some description'};
 
     beforeEach(mocks.module('biobankApp'));
 
-    beforeEach(inject(function($controller, $rootScope, $filter) {
+    beforeEach(inject(function($q, $controller, $rootScope, $filter, _studiesService_, _modalService_) {
       scope = $rootScope.$new();
+      studiesService = _studiesService_;
+      modalService = _modalService_;
+
+      spyOn(modalService, 'showModal').and.callFake(function () {
+        var deferred = $q.defer();
+        deferred.resolve('modalResult');
+        return deferred.promise;
+      });
 
       $controller('StudySummaryTabCtrl as vm', {
         $scope:  scope,
@@ -25,10 +33,45 @@ define(['angular', 'angularMocks', 'underscore', 'biobankApp'], function(angular
       expect(scope.vm.descriptionToggleLength).toBeDefined();
     });
 
-    xit('should change the status for a study', function() {
+    describe('change study status', function() {
+
+      beforeEach(inject(function($q) {
+        spyOn(studiesService, 'get').and.callFake(function () {
+          var deferred = $q.defer();
+          deferred.resolve(study);
+          return deferred.promise;
+        });
+      }));
+
+      function checkStatusChange($q, status) {
+        spyOn(studiesService, status).and.callFake(function () {
+          var deferred = $q.defer();
+          deferred.resolve('status changed');
+          return deferred.promise;
+        });
+
+        scope.vm.changeStatus(status);
+        scope.$digest();
+        expect(studiesService[status]).toHaveBeenCalledWith(study);
+      };
+
+      it('should enable a study', inject(function($q) {
+        checkStatusChange($q, 'enable');
+      }));
+
+      it('should disable a study', inject(function($q) {
+        checkStatusChange($q, 'disable');
+      }));
+
+      it('should retire a study', inject(function($q) {
+        checkStatusChange($q, 'retire');
+      }));
+
+      it('should unretire a study', inject(function($q) {
+        checkStatusChange($q, 'unretire');
+      }));
 
     });
-
 
   });
 
