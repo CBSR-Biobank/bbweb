1147aeeb048f7d05b61a96711dcedf31b050f9f9
Nelson Loyola <nloyola@gmail.com>
2015-02-03 13:38:13 -0700

    Fix so that panel is not collapsed.
    The fix is so that the panel is not collpsed when the user presses the
    'Add' button.

--- app/assets/javascripts/common/directives/panelButtonsDirective.js
+++ app/assets/javascripts/common/directives/panelButtonsDirective.js
@@ -1,26 +1,46 @@
 define(['../module'], function(module) {
   'use strict';
 
-  module.directive('panelButtons', panelButtons);
+  module.directive('panelButtons', panelButtonsDirective);
 
   /**
    * Displays a right justified button with a 'plus' icon. Meant to be used in a pane to add a
    * domain object.
    */
-  function panelButtons() {
+  function panelButtonsDirective() {
     var directive = {
       restrict: 'E',
-      replace: 'true',
+      replace: true,
       scope: {
-        add: '&onAdd',
+        onAdd: '&',
         addButtonTitle: '@',
         addButtonEnabled: '&',
         panelToggle: '&',
         panelOpen: '&'
       },
-      templateUrl: '/assets/javascripts/common/directives/panelButtons.html'
+      templateUrl: '/assets/javascripts/common/directives/panelButtons.html',
+      controller: 'PanelButtonsController as vm'
     };
     return directive;
+  }
+
+  module.controller('PanelButtonsController', PanelButtonsController);
+  PanelButtonsController.$inject =  ['$scope'];
+
+  /**
+   * This controller is needed to stop the propagation of the event generated when the 'Add' button
+   * is pressed. If the event was allowed to propgate, then the panel would be collapsed.
+   */
+  function PanelButtonsController($scope) {
+    var vm = this;
+
+    vm.addButtonPressed = addButtonPressed;
+
+    function addButtonPressed($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+      $scope.onAdd();
+    }
   }
 
 });
