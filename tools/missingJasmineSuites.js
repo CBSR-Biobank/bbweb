f86a530d4365edde20c80b2ee371a68a487372cc
Nelson Loyola <nloyola@gmail.com>
2016-06-18 13:02:37 -0600

    Converted script to ES6

--- tools/missingJasmineSuites.js
+++ tools/missingJasmineSuites.js
@@ -1,67 +1,60 @@
 #!/usr/bin/env node
 'use strict';
 
+/* jshint esversion:6 */
+
 /**
  * Displays a table of all the JavaScript files in the 'app' directory and whether or not there is a
  * Jasmine test suite for that file in the 'test' directory.
  */
 require('console.table');
 
-var program = require('commander'),
-    path    = require('path'),
-    fs      = require('fs'),
-    _       = require('lodash'),
-    sprintf = require('sprintf-js').sprintf;
+const program    = require('commander'),
+      path       = require('path'),
+      fs         = require('fs'),
+      _          = require('lodash'),
+      appfiles   = dirGetFiles('app/assets/javascripts'),
+      testfiles  = dirGetFiles('test/assets/javascripts'),
+      jsfiles    = getJsFiles(appfiles),
+      suitefiles = getJsSuiteFiles(testfiles);
 
 program.parse(process.argv);
-
-var appfiles = dirGetFiles('app/assets/javascripts'),
-    testfiles = dirGetFiles('test/assets/javascripts');
-
-var jsfiles = getJsFiles(appfiles),
-    suitefiles = getJsSuiteFiles(testfiles);
-
 checkForAllSuites(jsfiles, suitefiles);
 
 /**
  * Walks the directory tree looking for files.
  */
 function dirGetFiles(dir) {
-  var result;
+  let result;
 
   if (!fs.existsSync(dir)) {
     return [];
   }
 
   result = fs.readdirSync(dir)
-    .filter(function (f) {
-      return f && (f[0] !== '.'); // Ignore hidden files
-    })
-    .map(function (f) {
-      var p = path.join(dir, f),
+    .filter(f => f && (f[0] !== '.')) // Ignore hidden files
+    .map(f => {
+      let p = path.join(dir, f),
 	  stat = fs.statSync(p);
 
       if (stat.isDirectory()) {
 	return dirGetFiles(p);
       }
 
-      return path.join(p);
+      return p;
     });
 
   return _.flatMap(result);
 }
 
 function getJsFiles(files) {
-  return _.filter(files, function (item) {
-    return (item.match(/\.js$/) && !item.match(/(app|main|states).js/));
-  });
+  return _.filter(files, item => item.match(/\.js$/) && !item.match(/(app|main|states).js/));
 }
 
 function getJsSuiteFiles(files) {
-  return _.filter(getJsFiles(files), function (item) {
-    return (!item.match(/test\/assets\/javascripts\/test\/assets\/javascripts\/test/) &&
-            !item.match(/^test-main\.js$/));
-  });
+  return _.filter(getJsFiles(files), item =>
+                  !item.match(/test\/assets\/javascripts\/test\/assets\/javascripts\/test/) &&
+                  !item.match(/^test-main\.js$/));
 }
 
 function jsPathToSuitePath(jspath) {
@@ -69,14 +62,13 @@ function jsPathToSuitePath(jspath) {
 }
 
 function hasSuite(jspath, suitefiles) {
-  var suiteToFind = jsPathToSuitePath(jspath);
+  let suiteToFind = jsPathToSuitePath(jspath);
   //console.log('to find:', suiteToFind);
   return suitefiles.indexOf(suiteToFind) > 0;
 }
 
 function checkForAllSuites(jsfiles, suitefiles) {
-  var table = _.map(jsfiles, function (jsfile) {
-    return { suite: hasSuite(jsfile, suitefiles) ? 'Yes' : '', file: jsfile };
-  });
+  let table = _.map(jsfiles, jsfile =>
+                    ({ suite: hasSuite(jsfile, suitefiles) ? 'Yes' : '', file: jsfile }));
   console.table(table);
 }
