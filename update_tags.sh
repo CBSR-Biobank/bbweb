dd1f21944841ed6c99c2c94f6b060e93cad59699
Nelson Loyola <nloyola@gmail.com>
2016-09-07 12:01:16 +0200

    Using universal ctags with Emacs

--- update_tags.sh
+++ update_tags.sh
@@ -3,12 +3,13 @@
 # this script requires GNU global to be installed
 
 TMPFILE="$(mktemp)"
-find app  -type f -name \*.scala -print > $TMPFILE
-find test -type f -name \*.scala -print >> $TMPFILE
-find app  -type f -name \*.js    -print >> $TMPFILE
-find test -type f -name \*.js    -print >> $TMPFILE
+find app  -type f -name \*.scala -o -name \*.js > $TMPFILE
+find test -type f -name \*.scala -o -name \*.js >> $TMPFILE
 
 echo "TMPFILE is: $TMPFILE"
 
-#ctags-exuberant -eL $TMPFILE
-gtags -v --gtagslabel=ctags -f $TMPFILE
+ctags --output-format=etags -eL $TMPFILE
+
+# - having problems setting up gnu global to use universal ctags
+# - commenting out for now
+#gtags -v --gtagslabel new-ctags -f $TMPFILE
