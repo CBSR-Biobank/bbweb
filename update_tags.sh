b290193cefd2ab40e75bfebb91bb481554b05c6c
Nelson Loyola <nloyola@gmail.com>
2017-01-16 10:29:19 +0100

    Mention Universal Ctags

--- update_tags.sh
+++ update_tags.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# this script requires GNU global to be installed
+# this script requires Universal Ctags to be installed
 
 TMPFILE="$(mktemp)"
 find app  -type f -name \*.scala -o -name \*.js > $TMPFILE
@@ -8,7 +8,7 @@ find test -type f -name \*.scala -o -name \*.js >> $TMPFILE
 
 echo "TMPFILE is: $TMPFILE"
 
-ctags --output-format=etags -eL $TMPFILE
+exctags --output-format=etags -eL $TMPFILE
 
 # - having problems setting up gnu global to use universal ctags
 # - commenting out for now
