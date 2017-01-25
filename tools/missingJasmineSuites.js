ceb4aa127e94ecbef00487c6fded1da9e3b7c4fb
Nelson Loyola <nloyola@gmail.com>
2017-01-25 23:19:56 +0100

    Fixed eslint warnings in missingJasmineSuites.js

--- tools/missingJasmineSuites.js
+++ tools/missingJasmineSuites.js
@@ -1,74 +1,78 @@
 #!/usr/bin/env node
-'use strict';
 
-/* jshint esversion:6 */
-
-/**
- * Displays a table of all the JavaScript files in the 'app' directory and whether or not there is a
- * Jasmine test suite for that file in the 'test' directory.
- */
-require('console.table');
+/* eslint-env es6 */
+/* global process */
+/* eslint no-sync: "off" */
+/* eslint no-console: "off" */
 
 const program    = require('commander'),
       path       = require('path'),
       fs         = require('fs'),
-      _          = require('lodash'),
-      appfiles   = dirGetFiles('app/assets/javascripts'),
-      testfiles  = dirGetFiles('test/assets/javascripts'),
-      jsfiles    = getJsFiles(appfiles),
-      suitefiles = getJsSuiteFiles(testfiles);
+      _          = require('lodash');
 
-program.parse(process.argv);
-checkForAllSuites(jsfiles, suitefiles);
+require('console.table');
 
-/**
- * Walks the directory tree looking for files.
- */
-function dirGetFiles(dir) {
-  let result;
+(function () {
+  'use strict';
 
-  if (!fs.existsSync(dir)) {
-    return [];
+  /**
+   * Displays a table of all the JavaScript files in the 'app' directory and whether or not there is a
+   * Jasmine test suite for that file in the 'test' directory.
+   */
+
+  const appfiles   = dirGetFiles('app/assets/javascripts'),
+        testfiles  = dirGetFiles('test/assets/javascripts'),
+        jsfiles    = getJsFiles(appfiles),
+        suitefiles = getJsSuiteFiles(testfiles);
+
+  program.parse(process.argv);
+  checkForAllSuites(jsfiles, suitefiles);
+
+  /*
+   * Walks the directory tree looking for files.
+   */
+  function dirGetFiles(dir) {
+    if (!fs.existsSync(dir)) {
+      return [];
+    }
+
+    const result = fs.readdirSync(dir)
+          .filter((f) => f && (f[0] !== '.')) // Ignore hidden files
+          .map((f) => {
+            const p = path.join(dir, f);
+            var stat = fs.statSync(p);
+
+            if (stat.isDirectory()) {
+              return dirGetFiles(p);
+            }
+
+            return p;
+          });
+
+    return _.flatMap(result);
   }
 
-  result = fs.readdirSync(dir)
-    .filter(f => f && (f[0] !== '.')) // Ignore hidden files
-    .map(f => {
-      let p = path.join(dir, f),
-	  stat = fs.statSync(p);
+  function getJsFiles(files) {
+    return _.filter(files, (item) => item.match(/\.js$/) && !item.match(/(app|main|states).js/));
+  }
 
-      if (stat.isDirectory()) {
-	return dirGetFiles(p);
-      }
+  function getJsSuiteFiles(files) {
+    return _.filter(getJsFiles(files), (item) =>
+                    !item.match(/test\/assets\/javascripts\/test\/assets\/javascripts\/test/) &&
+                    !item.match(/^test-main\.js$/));
+  }
 
-      return p;
-    });
+  function jsPathToSuitePath(jspath) {
+    return jspath.replace(/^app/, 'test').replace(/\.js$/, 'Spec.js');
+  }
 
-  return _.flatMap(result);
-}
+  function hasSuite(jspath, suitefiles) {
+    return suitefiles.indexOf(jsPathToSuitePath(jspath)) > 0;
+  }
 
-function getJsFiles(files) {
-  return _.filter(files, item => item.match(/\.js$/) && !item.match(/(app|main|states).js/));
-}
-
-function getJsSuiteFiles(files) {
-  return _.filter(getJsFiles(files), item =>
-                  !item.match(/test\/assets\/javascripts\/test\/assets\/javascripts\/test/) &&
-                  !item.match(/^test-main\.js$/));
-}
-
-function jsPathToSuitePath(jspath) {
-  return jspath.replace(/^app/, 'test').replace(/\.js$/, 'Spec.js');
-}
-
-function hasSuite(jspath, suitefiles) {
-  let suiteToFind = jsPathToSuitePath(jspath);
-  //console.log('to find:', suiteToFind);
-  return suitefiles.indexOf(suiteToFind) > 0;
-}
-
-function checkForAllSuites(jsfiles, suitefiles) {
-  let table = _.map(jsfiles, jsfile =>
-                    ({ suite: hasSuite(jsfile, suitefiles) ? 'Yes' : '', file: jsfile }));
-  console.table(table);
-}
+  function checkForAllSuites(jsfiles, suitefiles) {
+    const table = _.map(jsfiles, (jsfile) =>
+                      ({ suite: hasSuite(jsfile, suitefiles) ? 'Yes' : '', file: jsfile }));
+    console.table(table);
+  }
+})();
