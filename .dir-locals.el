3bbf9b410e78e062a40a1816c32e837f45f64111
Nelson Loyola <nloyola@gmail.com>
2018-05-04 11:31:03 -0600

    More Emacs config files

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ((nil . ((fill-column . 110)
          (eval . (progn
                    (use-package bbweb-project :load-path "~/.emacs.d")
-                   (use-package angularjs :load-path "~/.emacs.d"))))))
+                   (use-package angularjs :load-path "~/.emacs.d")
+                   (setq dumb-jump-default-project (projectile-project-root)))))))
