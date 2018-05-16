60277cdb66d013eb39cba2edb39739f3f098d8bb
Nelson Loyola <nloyola@gmail.com>
2018-05-16 11:23:52 -0600

    Separated TAGS files

--- update_tags.sh
+++ update_tags.sh
@@ -2,13 +2,18 @@
 
 # this script requires Universal Ctags to be installed
 
-TMPFILE="$(mktemp)"
-find app  -type f -name \*.scala -o -name \*.js > $TMPFILE
-find test -type f -name \*.scala -o -name \*.js >> $TMPFILE
+TMPFILE_SCALA="$(mktemp)"
+TMPFILE_JS="$(mktemp)"
+find app  -type f -name \*.scala > $TMPFILE_SCALA
+find test -type f -name \*.scala >> $TMPFILE_SCALA
+find app  -type f -name \*.js > $TMPFILE_JS
+find test -type f -name \*.js >> $TMPFILE_JS
 
-echo "TMPFILE is: $TMPFILE"
+echo "TMPFILE_SCALA is: $TMPFILE_SCALA"
+echo "TMPFILE_JS is: $TMPFILE_JS"
 
-exctags --output-format=etags -eL $TMPFILE
+exctags --output-format=etags -eL $TMPFILE_SCALA -f SCALA_TAGS
+exctags --output-format=etags -eL $TMPFILE_JS -f JS_TAGS
 
 # - having problems setting up gnu global to use universal ctags
 # - commenting out for now
